c3dbe5c12e4cb1d73389b09758eb9ee33fc89700
ijounjalkar <jounjalkarisha@gmail.com>
2020-10-27 21:52:58 +0530

    First commit

---
+++ src/test/java/CabInvoiceGenerator/InvoiceServiceTest.java
@@ -0,0 +1,30 @@
+package CabInvoiceGenerator;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class InvoiceServiceTest {
+	
+	/**
+	 * UC1_CalucalteFare
+	 */
+	@Test
+	public void givenDistanceAndTime_shouldReturnTotalFare() {
+		InvoiceGenerator test  = new InvoiceGenerator();
+		double dist = 2.0;
+		int time = 5;
+		double fare = test.calculateFare(dist, time);
+		assertEquals(25, fare, 0.0);
+	}
+	
+	@Test
+	public void givenDistanceAndTime_shouldReturnMinimumFare() {
+		InvoiceGenerator test  = new InvoiceGenerator();
+		double dist = 0.0;
+		int time = 1;
+		double fare = test.calculateFare(dist, time);
+		assertEquals(5, fare);
+	}
+
+}
